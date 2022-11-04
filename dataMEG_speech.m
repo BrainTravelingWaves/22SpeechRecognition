53e95fc60c373e4d464acc7a765103b873f3d324
Vitaly Verkhlyutov <braintravelingwaves@gmail.com>
2022-11-04 19:45:34 +0300

    Update dataMEG_speech.m

--- dataMEG_speech.m
+++ dataMEG_speech.m
@@ -1,2 +1,2 @@
 % ksl-healthy right-handed male 24 years old, MEG-m1,m2,m3 (1000 ms after speeh stimulus) MEG sensor-s  
-https://drive.google.com/drive/folders/1u1cb-05ztPgY8J6T3ZxPvHFjLCAn43HZ?usp=share_link
+% https://drive.google.com/drive/folders/1u1cb-05ztPgY8J6T3ZxPvHFjLCAn43HZ?usp=share_link
